178a41177c4dd703b71ff182c94d2ac48e45d5ff
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2025-05-04 23:27:17 +0530

    fix: update robots rules structure for improved clarity and organization

--- portfolio/src/app/robots.ts
+++ portfolio/src/app/robots.ts
@@ -2,11 +2,27 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/private/',
+          '/api/',
+          '/_next/',
+          '/404',
+        ],
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: ['/assets/'],
+      },
+      {
+        userAgent: 'AdsBot-Google',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://sanjaykv.com/sitemap.xml',
+    host: 'https://sanjaykv.com',
   }
 }
